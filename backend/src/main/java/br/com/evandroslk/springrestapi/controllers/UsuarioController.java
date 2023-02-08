bb968e14695a4f57f2ac0429e9f2499940a28a83
Evandro <096066330647@tre-pr.jus.br>
2023-02-08 15:01:24 -0300

    Retornar usuário por id e tratar erro 404 - Não encontrado

--- backend/src/main/java/br/com/evandroslk/springrestapi/controllers/UsuarioController.java
+++ backend/src/main/java/br/com/evandroslk/springrestapi/controllers/UsuarioController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.evandroslk.springrestapi.dto.UsuarioDTO;
-import br.com.evandroslk.springrestapi.dto.UsuarioService;
+import br.com.evandroslk.springrestapi.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
@@ -20,10 +21,14 @@ public class UsuarioController {
 
 	@GetMapping
 	public ResponseEntity<List<UsuarioDTO>> findAll() {
-
 		List<UsuarioDTO> lista = service.findAll();
 		return ResponseEntity.ok().body(lista);
+	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id) {
+		UsuarioDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/br/com/evandroslk/springrestapi/services/UsuarioService.java
+++ backend/src/main/java/br/com/evandroslk/springrestapi/services/UsuarioService.java
@@ -1,13 +1,16 @@
-package br.com.evandroslk.springrestapi.dto;
+package br.com.evandroslk.springrestapi.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.evandroslk.springrestapi.dto.UsuarioDTO;
 import br.com.evandroslk.springrestapi.entities.Usuario;
 import br.com.evandroslk.springrestapi.repositories.UsuarioRepository;
+import br.com.evandroslk.springrestapi.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -19,6 +22,12 @@ public class UsuarioService {
 		List<Usuario> lista = repository.findAll();
 		return lista.stream().map(x -> new UsuarioDTO(x))
 				.collect(Collectors.toList());
+	}
+
+	public UsuarioDTO findById(Long id) {
+		Optional<Usuario> obj = repository.findById(id);
+		Usuario usuario = obj.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+		return new UsuarioDTO(usuario);
 	}
 
 }
