c984a478b22f90d51383409c4a674d743d2aa5d7
Evandro <096066330647@tre-pr.jus.br>
2023-02-08 14:02:38 -0300

    Teste de passagem de parametros e criacao da entidade Usuário

---
+++ src/test/java/br/com/evandroslk/springrestapi/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evandroslk.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
